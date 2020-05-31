9ac1ae98fe24822df4a8f888885baee2acb8e8a7
Tom Woodward <twwoodward@me.com>
2020-05-31 09:25:07 -0400

    function word count and display
    need to make universal maybe if we have a bunch of these

--- js/ubc-cis-admin.js
+++ js/ubc-cis-admin.js
@@ -1,1 +1,26 @@
 console.log('foo gu')
+
+
+//from https://stackoverflow.com/questions/17909646/counting-and-limiting-words-in-a-textarea
+jQuery(document).ready(function() {
+	let limit = parseInt(document.getElementById('short-desc-limit').innerHTML);//get limit number
+    let usedDisplay = document.getElementById('short-desc-limit-used'); //place to show used
+    let used = document.getElementById('acf-field_5ed26754e352d').value.match(/\S+/g).length; //count length on load
+    usedDisplay.innerHTML = used;
+  jQuery("#acf-field_5ed26754e352d").on('keyup', function() {
+    var words = this.value.match(/\S+/g).length;
+    
+    if (words > limit) {
+      // Split the string on first 200 words and rejoin on spaces
+      var trimmed = jQuery(this).val().split(/\s+/, 200).join(" ");
+      // Add a space at the end to make sure more typing creates new words
+      jQuery(this).val(trimmed + " ");
+    }
+    else {
+       console.log(words)
+      usedDisplay.innerHTML = words;
+      //jQuery('#word_left').text(limit-words);
+    }
+  });
+}); 
+
