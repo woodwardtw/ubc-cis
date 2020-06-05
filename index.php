1fcdaf28c2256c28ec0d73bf13f3538462e6a45d
Tom Woodward <twwoodward@me.com>
2020-06-04 20:44:48 -0400

    remove theme sidebar addition

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ function  ubc_cis_remove_menus(){
       remove_menu_page( 'edit-comments.php' );          //Comments
       //remove_menu_page( 'themes.php' );                 //Appearance
       //remove_menu_page( 'plugins.php' );                //Plugins
-      //remove_menu_page( 'users.php' );                  //Users
+      remove_menu_page( 'edit.php?post_type=portfolio' );   // remove that theme menu addition               
       remove_menu_page( 'tools.php' );                  //Tools
       remove_menu_page( 'options-general.php' );        //Settings
     }
